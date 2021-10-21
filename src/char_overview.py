62673a273e7bf8a9278143fc176329a721bd49be
Henry Ho <z5312521@ad.unsw.edu.au>
2021-10-21 12:46:02 +1100

    completed attack data

--- src/char_overview.py
+++ src/char_overview.py
@@ -13,10 +13,8 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 outer_div = soup.find(id="mw-content-text")
 
-# print(outer_div.prettify())
-
 for pair in outer_div.find_all(lambda tag: tag.name== "h3"):
-    if (pair.big.text.strip() == "j.S"):
+    # if (pair.big.text.strip() == "j.S"):
         # big json set
         moveData = {}
         
@@ -29,38 +27,69 @@ for pair in outer_div.find_all(lambda tag: tag.name== "h3"):
 
         # get the imgs and their descriptions
         imgDict = {}
-        imgBox = description.find("ul", class_="gallery mw-gallery-nolines")
-        for imgs in imgBox.find_all("li"):
-            # print(imgs.prettify())
-            imgSrc = imgBox.find("img")['src']
-            imgDescription = "img"
-            if imgs.find("p"):
-                imgDescription = imgBox.find("p").text.strip()
+        hitboxDict = {}
 
+        imgBox = description.find("ul", class_="gallery mw-gallery-nolines")
+        for img in imgBox.find_all("li"):
+            imgSrc = img.find("img")['src']
+            imgDescription = "img"
+            if img.find("p"):
+                imgDescription = img.find("p").text.strip()
+            
             imgDict[imgDescription] = imgSrc    
         moveData['imgs'] = imgDict
         
-        # get the table
-        atkData = {}
-        atkTable = description.find("table", class_="wikitable attack-data")
+        hitBox = imgBox.find_next("ul", class_="gallery mw-gallery-nolines")
 
-        atkHeaders = atkTable.find_all("th")
-        for header in atkHeaders:
-            if len(header.find_all()) == 0:
-                # just a header
-                # header = header.text.strip()
-                atkData[header.text.strip()] = atkTable.find_next('td').text.strip()
-            else:
-                # header = header.find('a').text.strip()
-                atkData[header.find('a').text.strip()] = atkTable.find_next('td').text.strip()
-                # a tag inside 
+        for img in hitBox.find_all("li"):
+            imgSrc = img.find("img")['src']
+            imgDescription = "img"
+            if img.find("p"):
+                imgDescription = img.find("p").text.strip()
+            
+            hitboxDict[imgDescription] = imgSrc    
+
+        moveData['hitboxes'] = hitboxDict
         
 
-        pprint.pprint(atkHeaders)
-        # atkData['damage'] = atkTable.find_next('td').text.strip()
-        # atkData['guard'] = atkTable.find_next('td').text.strip()
+        # get the table
+        atkTable = description.find("table", class_="wikitable attack-data")
 
-        rawValues = atkTable.find_all("td")
-        pprint.pprint(rawValues)
+        # form a list for headers
+        headings = []
+        atkHeaders = atkTable.find_all("th")
+        
+        for header in atkHeaders:
+            if len(header.find_all('span')) != 0:
+                header.span.span.extract()
+                headings.append(header.select_one('span[class="tooltip"]').text.strip())
+            elif len(header.find_all('a')) != 0:
+                headings.append(header.find('a').text.strip()) 
+            elif len(header.find_all()) == 0:
+                headings.append(header.text.strip()) 
+
+        # form a list of values    
+        data = atkTable.find_all('td')
+        cleaned_data = []
+
+        for each in data:
+            cleaned_data.append(each.text.strip())
+        
+        # merge together
+        atkData = dict(zip(headings,cleaned_data))
         moveData['atkInfo'] = atkData
+
+        propData = []
+        properties = description.find(class_='attack-info').find_all('li')
+        for prop in properties:
+            propData.append(prop.text.strip())
+        
+        moveData['properties'] = propData
+
+        notes = ''
+        for p in description.find(class_='attack-info').find_all('p'):
+            notes = notes + p.text.strip() + "\n"
+
+        moveData['notes'] = notes
+
         pprint.pprint(moveData)
